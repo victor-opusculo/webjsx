1ff10340be71f1b60ca41e5aaf52071cce27bc46
Jeswin Kumar <jeswinpk@agilehead.com>
2024-12-15 16:44:54 +0530

    Fix types

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -1,11 +1,13 @@
 import type * as JSXTypes from "./jsxTypes.js";
-import { Fragment, VNode, FragmentType } from "./types.js";
+import { FragmentType } from "./types.js";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements extends JSXTypes.IntrinsicElements {}
-    interface ElementAttributesProperty extends JSXTypes.ElementAttributesProperty {}
-    interface ElementChildrenAttribute extends JSXTypes.ElementChildrenAttribute {}
+    interface ElementAttributesProperty
+      extends JSXTypes.ElementAttributesProperty {}
+    interface ElementChildrenAttribute
+      extends JSXTypes.ElementChildrenAttribute {}
     interface Element {
       type: string | FragmentType;
       props: any;
