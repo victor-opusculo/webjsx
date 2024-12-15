c9108267fe6252f63ba4302297d73324a4d2ed19
Jeswin Kumar <jeswinpk@agilehead.com>
2024-12-15 17:09:00 +0530

    Fix index. up version.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { applyDiff } from "./applyDiff.js";
 export { createNode } from "./createNode.js";
 export { Fragment } from "./types.js";
 export * from "./types.js";
-export * from "./jsx.js";
