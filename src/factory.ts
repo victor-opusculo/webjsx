8c045bb613023ed757cb84dac6e66598053b41d0
Jeswin Kumar <jeswinpk@agilehead.com>
2024-12-15 21:24:24 +0530

    support jsxFactory

---
+++ src/factory.ts
@@ -0,0 +1,5 @@
+import { createElement } from "./createElement.js";
+import { Fragment } from "./types.js";
+export * from "./jsx.js";
+export { createElement, Fragment };
+export default createElement;
